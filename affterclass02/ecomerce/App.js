3d20972e007de5f4d80368f5fc8ffcdffd72fe6f
Gonzalez Adrian <adrgon@gmail.com>
2025-03-28 19:49:22 -0300

    Inicio affterclass02

---
+++ affterclass02/ecomerce/App.js
@@ -0,0 +1,22 @@
+
+import { StyleSheet, View } from 'react-native';
+
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 100,
+  },
+});
