ed7039ae35cab058ef137ab03b103d6ba0f3de66
Gonzalez Adrian <adrgon@gmail.com>
2025-04-16 22:43:49 -0300

    Clase 10 - finalizada

--- clase10/ecomerce/src/navigation/Navigator.jsx
+++ clase10/ecomerce/src/navigation/Navigator.jsx
@@ -1,22 +1,13 @@
 
 import React, {useState} from 'react'
-
-import Home from "../screens/Home";
-import ItemListCategory from "../screens/ItemListCategory";
-import ItemDetail from "../screens/ItemDetail";
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import BottomTabNavigator from './BottomTabNavigator';
+import { Text } from 'react-native';
 
-const Stack = createNativeStackNavigator()
 const Navigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen component={Home} name="Home" />      
-        <Stack.Screen component={ItemListCategory} name="ItemListCategory" />
-        <Stack.Screen component={ItemDetail} name="ItemDetail" /> 
-      </Stack.Navigator>
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 }
