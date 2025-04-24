d25402c8a7d7acb1929577acdb623bc9b8a2ae7a
Gonzalez Adrian <adrgon@gmail.com>
2025-04-23 21:01:05 -0300

    Inicio clase11

---
+++ clase12/ecomerce/App.js
@@ -0,0 +1,36 @@
+import { colors } from "./src/global/colors";
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import Navigator from "./src/navigation/Navigator";
+import { Provider } from "react-redux";
+import store from "./src/store";
+
+const App = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    Josefin: require("./assets/JosefinSans-Regular.ttf"),
+  });
+
+  if (!fontsLoaded || fontError) {
+    return null;
+  }
+
+  if (fontsLoaded && !fontError) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Provider store={store}>
+          <Navigator />
+        </Provider>
+      </SafeAreaView>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+    backgroundColor: colors.teal200,
+  },
+});
+
+export default App;
---
+++ clase12/ecomerce/src/store/index.js
@@ -0,0 +1,9 @@
+const { configureStore } = require("@reduxjs/toolkit");
+import counterReducer from "../features/Counter/counterSlice"
+import shopReducer from "../features/Shop/shopSlice"
+export default configureStore({
+  reducer: {
+    counter: counterReducer,
+    shop: shopReducer,
+  },
+});
